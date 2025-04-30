1483a8fc4898d735a8b591de14d13f5e50cf7ce3
israel.sturaro <israel.sturaro@cialne.com.br>
2025-04-30 14:03:47 -0300

    fix: corrige a extração do userId no JwtStrategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -18,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   validate(payload: JwtPayload) {
     return {
-      userId: payload.sub,
+      userId: payload.id,
       email: payload.email,
       username: payload.username,
       role: payload.role,
